cb1467d9fb77858ef08cfff3c29cd9ac06a33d65
SimonPislar <51m0np15l3r@gmail.com>
2023-11-20 15:02:48 +0100

    Add authentication check when loading page

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,26 @@
 import React from "react";
-import './CSS/Home.css';
+import './ReactComponents/CSS/Home.css';
 import HomeTemplate from "./HomeTemplate";
 
 function Home() {
+
+    const formData = new URLSearchParams();
+    formData.append('sessionToken', localStorage.getItem('sessionToken'));
+    formData.append('email', localStorage.getItem('email'));
+    fetch ('http://localhost:8080/user-controller/check-session-token', {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }).then((response) => response.text())
+        .then((data) => {
+                if (data === "false") {
+                    window.location.href = "/";
+                }
+            }
+        );
+
     return (
         <HomeTemplate />
     );
