99077b549ab4d0c1b50d97925e6e634e3d45bb24
SimonPislar <51m0np15l3r@gmail.com>
2023-11-20 15:03:18 +0100

    Remove white box on homepage, add basic navbar

--- src/HomeTemplate.js
+++ src/HomeTemplate.js
@@ -1,14 +1,22 @@
-import react from "react";
-import './CSS/HomeTemplate.css';
+import './ReactComponents/CSS/HomeTemplate.css';
 
 function HomeTemplate() {
     return (
         <div class="home-template-container">
-            <div className="white-box">
-                <h1>Home</h1>
-            </div>
+            <nav>
+                <a href="#" onClick={handle_log_out}>Log out</a>
+                <a href="#">About</a>
+                <a href="#">Services</a>
+                <a href="#">Contact</a>
+            </nav>
         </div>
     );
+}
+
+const handle_log_out = () => {
+    console.log("Log out button clicked");
+    localStorage.clear();
+    window.location.href = "/";
 }
 
 export default HomeTemplate;
